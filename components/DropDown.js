e71f7524b13ed825508910dcba2ff5ebba30ec8b
Pallab6Sense <pallab@6sensehq.com>
2023-03-11 22:16:54 +0600

    collapsible items done

--- components/DropDown.js
+++ components/DropDown.js
@@ -1,10 +1,45 @@
 /* eslint-disable @next/next/no-img-element */
+import * as React from 'react';
+
+import { useState } from 'react';
 import dropdownStyle from '../styles/DropDown.module.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+
 export default function DropDown() {
+  const [iscollapse, setIsCollapse] = useState(true);
+  const [iscollapse1, setIsCollapse1] = useState(false);
+  const [iscollapse2, setIsCollapse2] = useState(false);
+
+  function handleClick() {
+    if (iscollapse == true) {
+    } else {
+      setIsCollapse(true);
+      setIsCollapse1(false);
+      setIsCollapse2(false);
+    }
+  }
+
+  function handleClick1() {
+    if (iscollapse1 == true) {
+    } else {
+      setIsCollapse1(true);
+      setIsCollapse(false);
+      setIsCollapse2(false);
+    }
+  }
+
+  function handleClick2() {
+    if (iscollapse2 == true) {
+    } else {
+      setIsCollapse2(true);
+      setIsCollapse1(false);
+      setIsCollapse(false);
+    }
+  }
+
   return (
     <>
       <div className={dropdownStyle.container}>
-
         <div className={dropdownStyle.heading}>
           <p className={dropdownStyle.advantages}>
             Advantages Of Choosing <span>6sense</span>{' '}
@@ -17,25 +52,62 @@ export default function DropDown() {
         </div>
 
         <div className={dropdownStyle.collapse}>
-            <div className={dropdownStyle.collapseDiv}>
-          <div className={dropdownStyle.collapseItem}>
-            <div className={dropdownStyle.items}>
-                <p className={dropdownStyle.head}>Cost Savings By 50%</p>
-                <p className={dropdownStyle.description}>By working with our custom software company, our clients can save at least 50% on their software development costs. Our personalized solutions are designed specifically for the needs and goals of each client, ensuring that they get the most value for their investment. We work closely with our clients to understand their unique needs and create software that fits their workflow and processes, helping to streamline operations and improve efficiency.</p>
+          <div className={dropdownStyle.collapseDiv}>
+            <div className={dropdownStyle.collapseItem}>
+              <div className={dropdownStyle.items}>
+                <p className={dropdownStyle.head} onClick={handleClick}>
+                  Cost Savings By 50% <span>&or;</span>
+                  <i class="fa-solid fa-chevron-down"></i>
+                </p>
+                {iscollapse && (
+                  <p className={dropdownStyle.description}>
+                    By working with our custom software company, our clients can
+                    save at least 50% on their software development costs. Our
+                    personalized solutions are designed specifically for the
+                    needs and goals of each client, ensuring that they get the
+                    most value for their investment. We work closely with our
+                    clients to understand their unique needs and create software
+                    that fits their workflow and processes, helping to
+                    streamline operations and improve efficiency.
+                  </p>
+                )}
+              </div>
             </div>
-          </div>
-          <div className={dropdownStyle.collapseItem}>
-            <div className={dropdownStyle.items}>
-                <p className={dropdownStyle.head}>Ability to integrate with existing systems and processes</p>
-                <p className={dropdownStyle.description}>We have the ability to seamlessly integrate with our clients’ existing systems and processes. This means that our software can work alongside their current technology and platforms, rather than requiring them to completely overhaul their systems. This not only saves time and resources, but also helps to minimize disruptions to their business operations..</p>
+            <div className={dropdownStyle.collapseItem}>
+              <div className={dropdownStyle.items}>
+                <p className={dropdownStyle.head} onClick={handleClick1}>
+                  Ability to integrate with existing systems and processes <span>&or;</span>
+                </p>
+                {iscollapse1 && (
+                  <p className={dropdownStyle.description}>
+                    We have the ability to seamlessly integrate with our
+                    clients’ existing systems and processes. This means that our
+                    software can work alongside their current technology and
+                    platforms, rather than requiring them to completely overhaul
+                    their systems. This not only saves time and resources, but
+                    also helps to minimize disruptions to their business
+                    operations..
+                  </p>
+                )}
+              </div>
             </div>
-          </div>
-          <div className={dropdownStyle.collapseItem}>
-            <div className={dropdownStyle.items}>
-                <p className={dropdownStyle.head}>Improved data management</p>
-                <p className={dropdownStyle.description}>By designing software that fits the specific workflow and processes of the client, we can help streamline data entry and organization, making it easier to access and use. In addition, our custom software can be designed with robust security measures in place to protect sensitive data and prevent unauthorized access.</p>
+            <div className={dropdownStyle.collapseItem}>
+              <div className={dropdownStyle.items}>
+                <p className={dropdownStyle.head} onClick={handleClick2}>
+                  Improved data management <span>&or;</span>
+                </p>
+                {iscollapse2 && (
+                  <p className={dropdownStyle.description}>
+                    By designing software that fits the specific workflow and
+                    processes of the client, we can help streamline data entry
+                    and organization, making it easier to access and use. In
+                    addition, our custom software can be designed with robust
+                    security measures in place to protect sensitive data and
+                    prevent unauthorized access.
+                  </p>
+                )}
+              </div>
             </div>
-          </div>
           </div>
           <div className={dropdownStyle.rightImage}>
             <img src="Choosing-6sense.png" alt="" />
