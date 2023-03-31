b615815a1b68132041af2cbbc9a81b94977574ef
Pallab6Sense <pallab@6sensehq.com>
2023-03-31 12:45:38 +0600

    Fixed the transation in 1st accordin

--- components/DropDown.js
+++ components/DropDown.js
@@ -1,6 +1,11 @@
 /* eslint-disable @next/next/no-img-element */
 import { useCallback, useState } from 'react';
 import dropdownStyle from '../styles/DropDown.module.css';
+import Accordion from '@mui/material/Accordion';
+import AccordionDetails from '@mui/material/AccordionDetails';
+import AccordionSummary from '@mui/material/AccordionSummary';
+import Typography from '@mui/material/Typography';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 
 export default function DropDown() {
   const [iscollapse, setIsCollapse] = useState(true);
@@ -37,6 +42,12 @@ export default function DropDown() {
     }
   }, [iscollapse2]);
 
+  const [expanded, setExpanded] = useState(false);
+
+  const handleChange = (panel) => (event, isExpanded) => {
+    setExpanded(isExpanded ? panel : false);
+  };
+
   return (
     <>
       <div className={dropdownStyle.container}>
@@ -53,62 +64,68 @@ export default function DropDown() {
 
         <div className={dropdownStyle.collapse}>
           <div className={dropdownStyle.collapseDiv}>
-            <div className={dropdownStyle.collapseItem}>
-              <div className={dropdownStyle.items}>
-                <p className={dropdownStyle.head} onClick={handleClick}>
-                  Cost Savings By 50% {!iscollapse ? <span>&or;</span> : null}
-                </p>
-                {iscollapse ? (
-                  <p className={dropdownStyle.description}>
-                    By working with our custom software company, our clients can
-                    save at least 50% on their software development costs. Our
-                    personalized solutions are designed specifically for the
-                    needs and goals of each client, ensuring that they get the
-                    most value for their investment. We work closely with our
-                    clients to understand their unique needs and create software
-                    that fits their workflow and processes, helping to
-                    streamline operations and improve efficiency.
-                  </p>
-                ) : null}
-              </div>
-            </div>
-            <div className={dropdownStyle.collapseItem}>
-              <div className={dropdownStyle.items}>
-                <p className={dropdownStyle.head} onClick={handleClick1}>
-                  Ability to integrate with existing systems and processes{' '}
-                  {!iscollapse1 ? <span>&or;</span> : null}
-                </p>
-                {iscollapse1 ? (
-                  <p className={dropdownStyle.description}>
-                    We have the ability to seamlessly integrate with our
-                    clients’ existing systems and processes. This means that our
-                    software can work alongside their current technology and
-                    platforms, rather than requiring them to completely overhaul
-                    their systems. This not only saves time and resources, but
-                    also helps to minimize disruptions to their business
-                    operations..
-                  </p>
-                ) : null}
-              </div>
-            </div>
-            <div className={dropdownStyle.collapseItem}>
-              <div className={dropdownStyle.items}>
-                <p className={dropdownStyle.head} onClick={handleClick2}>
-                  Improved data management{' '}
-                  {!iscollapse2 ? <span>&or;</span> : null}
-                </p>
-                {iscollapse2 ? (
-                  <p className={dropdownStyle.description}>
-                    By designing software that fits the specific workflow and
-                    processes of the client, we can help streamline data entry
-                    and organization, making it easier to access and use. In
-                    addition, our custom software can be designed with robust
-                    security measures in place to protect sensitive data and
-                    prevent unauthorized access.
-                  </p>
-                ) : null}
-              </div>
-            </div>
+            <Accordion
+              expanded={iscollapse}
+              onChange={handleChange('panel1')}
+            >
+              <AccordionSummary className={dropdownStyle.items}
+                onClick={handleClick}
+                expandIcon={!iscollapse ? <ExpandMoreIcon /> : null}
+                aria-controls="panel1bh-content"
+                id="panel1bh-header"
+              >
+                <Typography className={dropdownStyle.head}>
+                Cost Savings By 50%
+                </Typography>
+              </AccordionSummary>
+              <AccordionDetails >
+                <Typography className={dropdownStyle.description}>
+                By working with our custom software company, our clients can save at least 50% on their software development costs. Our personalized solutions are designed specifically for the needs and goals of each client, ensuring that they get the most value for their investment. We work closely with our clients to understand their unique needs and create software that fits their workflow and processes, helping to streamline operations and improve efficiency.
+                </Typography>
+              </AccordionDetails>
+            </Accordion>
+            <Accordion
+              expanded={expanded === 'panel2'}
+              onChange={handleChange('panel2')}
+            >
+              <AccordionSummary className={dropdownStyle.items}
+                onClick={handleClick1}
+                expandIcon={!iscollapse1 ? <ExpandMoreIcon /> : null}
+                aria-controls="panel2bh-content"
+                id="panel2bh-header"
+              >
+                
+                <Typography className={dropdownStyle.head}>
+                Ability to integrate with existing systems and processes
+                </Typography>
+              </AccordionSummary>
+              <AccordionDetails>
+                <Typography className={dropdownStyle.description}>
+                We have the ability to seamlessly integrate with our clients’ existing systems and processes. This means that our software can work alongside their current technology and platforms, rather than requiring them to completely overhaul their systems. This not only saves time and resources, but also helps to minimize disruptions to their business operations.
+                </Typography>
+              </AccordionDetails>
+            </Accordion>
+            <Accordion
+              expanded={expanded === 'panel3'}
+              onChange={handleChange('panel3')}
+            >
+              <AccordionSummary className={dropdownStyle.items}
+              onClick={handleClick2}
+              expandIcon={!iscollapse2 ? <ExpandMoreIcon /> : null}
+                aria-controls="panel3bh-content"
+                id="panel3bh-header"
+              >
+              
+                <Typography className={dropdownStyle.head}>
+                Improved data management
+                </Typography>
+              </AccordionSummary>
+              <AccordionDetails>
+                <Typography className={dropdownStyle.description}>
+                By designing software that fits the specific workflow and processes of the client, we can help streamline data entry and organization, making it easier to access and use. In addition, our custom software can be designed with robust security measures in place to protect sensitive data and prevent unauthorized access.
+                </Typography>
+              </AccordionDetails>
+            </Accordion>
           </div>
           <div className={dropdownStyle.rightImage}>
             <img src="Choosing-6sense.png" alt="" />
